f624c27cde90d617fd7fe14ba756e18fc1ef8b6b
Manfred <Manfred@manfred047.com>
2018-10-29 11:01:30 -0600

    Se agregan comentarios

--- src/api/UserResource.java
+++ src/api/UserResource.java
@@ -16,46 +16,84 @@ import java.util.HashMap;
 
 public class UserResource extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    // Se genera de forma aleatoria, establece version de la clase
+    private static final long serialVersionUID = 8057734212103116679L;
 
+    /**
+     * Funcion que se ejecuta en las peticiones de tipo "GET"
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // Se establece cabecera de tipo de respuesta
         response.setContentType("application/json; charset=utf-8");
+        // Obtener instancia del response
         PrintWriter out = response.getWriter();
 
+        // Crear arreglo de objetos
         ArrayList obj = new ArrayList<>();
 
+        // Generar datos aleatorios para la tabla
         for (int i = 0; i <= 10; i++) {
+            // Llamada a la clase de UserModel
             obj.add(new UserModel("Sara" + i, "Von" + i, i));
         }
 
+        // Llamada a la clase de UserDataModel
         UserDataModel userDataModel = new UserDataModel();
+        // Anexar arreglo de clase UserModel a UserDataModel
         userDataModel.setData(obj);
 
+        // Instanciar la clase de JSON (libreria externa de google).
         Gson gson = new Gson();
+        // Convertir el arreglo a formato json
         String json = gson.toJson(userDataModel);
 
-        out.print(json);
-        out.flush();
+        // Enviar la respuesta al navegador
+        out.write(json);
+        // vaciar la instancia
+        out.close();
 
     }
 
+    /**
+     * Funcion que se ejecuta para peticiones de tipo "POST"
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // Se establece cabecera de tipo de respuesta
         response.setContentType("application/json; charset=utf-8");
+        // Obtener instancia del response
         PrintWriter out = response.getWriter();
 
+        // Obtener instancia de lectura
         BufferedReader reader = request.getReader();
+
+        // Instanciar la clase de JSON (libreria externa de google).
         Gson gson = new Gson();
 
+        // Convierte los parametros recibidos de tipo post a tipo compatible con la clase UserModel
         UserModel userModel = gson.fromJson(reader, UserModel.class);
+        // Crear objeto de respuesta
         HashMap<String, Object> map = new HashMap<>();
+        // Anexar a la variable "data" los valores de UserModel
         map.put("data", userModel);
-
+        // Convertir el objeto a formato json
         String json = gson.toJson(map);
 
-        out.print(json);
-        out.flush();
+        // Enviar la respuesta al navegador
+        out.write(json);
+        // vaciar la instancia
+        out.close();
 
     }
 
--- web/js/app.js
+++ web/js/app.js
@@ -1,28 +1,37 @@
 const app = new Vue({
     el: '#app',
     methods: {
+        /**
+         * Solicitar informacion al servidor
+         */
         getData: function() {
+            // Guardar la instancia a una variable
             var self = this;
             axios.get("/users")
                 .then(function (response) {
+                    // cargar datos al arreglo de users (usando la variable local creada
                     self.users = response.data.data;
                 })
                 .catch(function (errors) {
+                    // Si algo salio mal
                     console.log(errors);
                     alert('Error al optener la informacion');
                 });
         },
         store: function () {
+            // Guardar la instancia a una variable
             var self = this;
             axios.post("/users", self.form)
                 .then(function (response) {
 
                     if (self.users.length) {
+                        // Si el arreglo ya contiene datos
                         self.users.push(response.data.data);
                     } else {
+                        // si el arreglo es vacio
                         self.users = [response.data.data];
                     }
-
+                    // Limpiar formulario
                     self.form = {
                         name: "",
                         lastName: "",
@@ -32,11 +41,13 @@ const app = new Vue({
                 })
                 .catch(function (errors) {
                     console.log(errors);
+                    // si algo salio mal
                     alert("Error inesperado");
                 });
         }
     },
     data: function() {
+        // Variables que contienen informacion
         return {
             message: "Hola :3",
             formMessage: "Agregar desde formulario",
